adbad684a741a5b0726a29849e590768208687af
john-guo <guoyichao@hotmail.com>
2016-02-24 12:09:51 +0800

    WOLTEST

---
+++ WOLTest/ARPQuery/ARP.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ARPQuery
+{
+    public class ARP
+    {
+        [DllImport("Iphlpapi.dll")]
+        public static extern uint SendARP(uint DestIP, uint SrcIP, ref ulong pMacAddr, ref uint PhyAddrLen);
+
+        public static string GetMac(string ip)
+        {
+            IPAddress address;
+            if (!IPAddress.TryParse(ip, out address)) return "";
+            uint DestIP = BitConverter.ToUInt32(address.GetAddressBytes(), 0);
+            ulong pMacAddr = 0;
+            uint PhyAddrLen = 6;
+            uint error_code = SendARP(DestIP, 0, ref pMacAddr, ref PhyAddrLen);
+            byte[] bytes = BitConverter.GetBytes(pMacAddr);
+            return BitConverter.ToString(bytes, 0, 6);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                return;
+
+            Console.WriteLine("{0}", GetMac(args[0]));
+        }
+    }
+}
---
+++ WOLTest/WOLTest/nattest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NATUPNPLib;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace WOLTest
+{
+    public class NATTest
+    {
+        static string GetIP()
+        {
+            var link = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var ni in link)
+            {
+                if (ni.NetworkInterfaceType != NetworkInterfaceType.Loopback &&
+                    ni.OperationalStatus == OperationalStatus.Up &&
+                    ni.GetIPProperties().GatewayAddresses.Count > 0)
+                {
+                    foreach (var ai in ni.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ai.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            return ai.Address.ToString();
+                        }
+                    }
+                }
+            }
+
+            return String.Empty;
+        }
+
+
+        static void Main()
+        {
+            var upnp = new UPnPNAT();
+
+            var mappings = upnp.StaticPortMappingCollection;
+
+            if (mappings == null)
+                return;
+
+            mappings.Add(9, "UDP", 9, GetIP(), true, "test");
+        }
+    }
+}
