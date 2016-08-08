271d781d9c09ff9e8b7072901b188e491ebd4ba8
john-guo <guoyichao@hotmail.com>
2016-08-08 17:10:00 +0800

    try WPF

---
+++ WindowUtils/AWTest/TimeSource.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace AWTest
+{
+    public class TimeSource : INotifyPropertyChanged
+    {
+        public TimeSource()
+        {
+            var timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        public string NowTime
+        {
+            get
+            {
+                return DateTime.Now.ToLongTimeString();
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs("NowTime"));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
